0df33071f8d18c6e5554e61a65d35cdfa01894fd
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2018-08-06 10:31:48 +0200

    Add Unit tests for Coordinator using Robolectric

--- android/SwiftKotlination/app/src/main/java/fr/jhandguy/swiftkotlination/Coordinator.kt
+++ android/SwiftKotlination/app/src/main/java/fr/jhandguy/swiftkotlination/Coordinator.kt
@@ -8,29 +8,35 @@ import fr.jhandguy.swiftkotlination.features.story.view.StoryActivity
 import fr.jhandguy.swiftkotlination.features.topstories.view.TopStoriesActivity
 
 interface Coordinator {
-    fun start()
-    fun open(story: Story)
-    fun open(url: String)
+    fun start(): Intent
+    fun open(story: Story): Intent
+    fun open(url: String): Intent
     fun finish()
 }
 
 class CoordinatorImpl(val activity: Activity): Coordinator {
 
-    override fun start() {
+    override fun start(): Intent {
         val intent = Intent(activity, TopStoriesActivity::class.java)
         activity.startActivity(intent)
         activity.finish()
+
+        return intent
     }
 
-    override fun open(story: Story) {
+    override fun open(story: Story): Intent {
         val intent = Intent(activity, StoryActivity::class.java)
         intent.putExtra(story.javaClass.simpleName, story)
         activity.startActivity(intent)
+
+        return intent
     }
 
-    override fun open(url: String) {
+    override fun open(url: String): Intent {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         activity.startActivity(intent)
+
+        return intent
     }
 
     override fun finish() {
---
+++ android/SwiftKotlination/app/src/test/java/fr/jhandguy/swiftkotlination/CoordinatorUnitTest.kt
@@ -0,0 +1,70 @@
+package fr.jhandguy.swiftkotlination
+
+import android.app.Activity
+import android.content.Intent
+import fr.jhandguy.swiftkotlination.features.story.model.Story
+import fr.jhandguy.swiftkotlination.features.story.view.StoryActivity
+import fr.jhandguy.swiftkotlination.features.topstories.view.TopStoriesActivity
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.parameter.parametersOf
+import org.koin.standalone.StandAloneContext.closeKoin
+import org.koin.standalone.inject
+import org.koin.test.KoinTest
+import org.robolectric.Robolectric.setupActivity
+import org.robolectric.RobolectricTestRunner
+
+
+@RunWith(RobolectricTestRunner::class)
+class CoordinatorUnitTest: KoinTest {
+
+    lateinit var activity: Activity
+
+    val coordinator: Coordinator by inject { parametersOf(activity) }
+
+    @Before
+    fun before() {
+        activity = setupActivity(Activity::class.java)
+    }
+
+    @Test
+    fun `coordinator starts correctly`() {
+        val intent = coordinator.start()
+
+        assert(activity.isFinishing)
+        assertEquals(intent.component.className, TopStoriesActivity::class.java.name)
+    }
+
+    @Test
+    fun `coordinator opens story correctly`() {
+        val intent = coordinator.open(Story())
+
+        assert(!activity.isFinishing)
+        assertEquals(intent.component.className, StoryActivity::class.java.name)
+    }
+
+    @Test
+    fun `coordinator opens url correctly`() {
+        val url = "https//test.com"
+        val intent = coordinator.open(url)
+
+        assert(!activity.isFinishing)
+        assertEquals(intent.action, Intent.ACTION_VIEW)
+        assertEquals(intent.dataString, url)
+    }
+
+    @Test
+    fun `coordinator finishes correctly`() {
+        coordinator.finish()
+
+        assert(activity.isFinishing)
+    }
+
+    @After
+    fun after() {
+        closeKoin()
+    }
+}
