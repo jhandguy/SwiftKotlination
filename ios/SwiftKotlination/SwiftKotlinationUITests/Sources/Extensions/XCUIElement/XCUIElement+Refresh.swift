a269210046c63a26ae5cb6ec63991868647a4f7f
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2018-10-09 11:44:24 +0200

    Change testing device to iPhone XS Max

--- ios/SwiftKotlination/SwiftKotlinationUITests/Sources/Extensions/XCUIElement/XCUIElement+Refresh.swift
+++ ios/SwiftKotlination/SwiftKotlinationUITests/Sources/Extensions/XCUIElement/XCUIElement+Refresh.swift
@@ -3,7 +3,7 @@ import XCTest
 extension XCUIElement {
     func refresh() {
         let startCoordinate = cells.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
-        let finishCoordinate = cells.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 2))
+        let finishCoordinate = cells.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 3))
         startCoordinate.press(forDuration: 0, thenDragTo: finishCoordinate)
     }
 }
