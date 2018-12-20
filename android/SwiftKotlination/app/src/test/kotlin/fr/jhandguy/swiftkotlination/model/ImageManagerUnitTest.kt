f8b554ff9fd1923e0204907a37178c4563fbe377
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2018-12-20 09:53:55 +0100

    Remove ImageManagerUnitTest

--- android/SwiftKotlination/app/src/test/kotlin/fr/jhandguy/swiftkotlination/model/ImageManagerUnitTest.kt
+++ android/SwiftKotlination/app/src/test/kotlin/fr/jhandguy/swiftkotlination/model/ImageManagerUnitTest.kt
@@ -1,24 +1,7 @@
 package fr.jhandguy.swiftkotlination.model
 
-import kotlin.test.Test
-
 class ImageManagerUnitTest {
 
     lateinit var sut: ImageManager
-
-    @Test
-    fun `TODO`() {
-        /*val image = File("27arizpolitics7-thumbLarge", File.Extension.JPG).data!!.readBytes()
-        val networkManager = NetworkManagerMock(Result.Success(image))
-        sut = ImageManager(networkManager)
-
-        runBlocking {
-            sut.image("") { result ->
-                when(result) {
-                    is Result.Success -> assertEquals(result.data, BitmapFactory.decodeByteArray(image, 0, image.size))
-                    is Result.Failure -> fail(result.error.message)
-                }
-            }
-        }*/
-    }
+    
 }
