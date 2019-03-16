9f22201992901569b380a6f67986ebc664159071
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2019-03-16 14:58:25 +0100

    Prepare Main Activity UI Test

--- android/SwiftKotlination/app/src/androidTest/kotlin/fr/jhandguy/swiftkotlination/features/main/MainActivityTest.kt
+++ android/SwiftKotlination/app/src/androidTest/kotlin/fr/jhandguy/swiftkotlination/features/main/MainActivityTest.kt
@@ -1,10 +1,16 @@
 package fr.jhandguy.swiftkotlination.features.main
 
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.rule.ActivityTestRule
 import fr.jhandguy.swiftkotlination.AppMock
+import fr.jhandguy.swiftkotlination.features.main.view.MainActivity
+import org.junit.Rule
 import org.junit.Test
 
 class MainActivityTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun `test`() {
         val application = ApplicationProvider.getApplicationContext<AppMock>()
