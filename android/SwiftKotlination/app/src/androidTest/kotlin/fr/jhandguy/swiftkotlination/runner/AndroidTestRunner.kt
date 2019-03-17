31ca632690e68c4e1b362deb9ad82fb634b52376
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2019-03-17 20:09:04 +0100

    Add ktlint

--- android/SwiftKotlination/app/src/androidTest/kotlin/fr/jhandguy/swiftkotlination/runner/AndroidTestRunner.kt
+++ android/SwiftKotlination/app/src/androidTest/kotlin/fr/jhandguy/swiftkotlination/runner/AndroidTestRunner.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import fr.jhandguy.swiftkotlination.AppMock
 
-class AndroidTestRunner: AndroidJUnitRunner() {
+class AndroidTestRunner : AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
         val appMockName = AppMock::class.java.canonicalName
         return super.newApplication(cl, appMockName, context)
